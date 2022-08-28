468b7803638a8e15c2fb0b72498e0717ac162603
Nasim Mahdavi <55268101+nasimshm@users.noreply.github.com>
2022-08-28 00:24:21 -0500

    Create FB3-Multi_GPU.sh

---
+++ Experiments/FB3-Multi_GPU.sh
@@ -0,0 +1,40 @@
+###FB3_TransE_l2:
+dglke_train --model_name TransE_l2 --dataset Freebase --data_path ./data --format udd_hrt  \
+--data_files entity2id.txt relation2id.txt train.txt valid.txt test.txt --batch_size 1000 --neg_sample_size 200 --hidden_dim 400 --gamma 10 \
+--lr 0.1 --regularization_coef 1e-9 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 --num_thread 4 --gpu 0 1  --max_step 320000 \
+--neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_em --delimiter ,
+
+################## Script Result #################
+
+##################################################
+
+###FB3-DistMult:
+dglke_train --model_name DistMult --dataset Freebase --data_path ./data --format udd_hrt  \
+--data_files entity2id.txt relation2id.txt train.txt valid.txt test.txt  --batch_size 1024 --neg_sample_size 256 --hidden_dim 400 --gamma 143.0 \
+--lr 0.08 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 --gpu 0 1 --max_step 300000 --neg_sample_size_eval 1000 \
+--eval_interval 100000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --num_thread 4 --no_save_emb --delimiter ,
+
+################## Script Result #################
+
+##################################################
+
+###FB3-ComplEx:
+dglke_train --model_name ComplEx --dataset Freebase --data_path ./data --format udd_hrt  \
+--data_files entity2id.txt relation2id.txt train.txt valid.txt test.txt  --batch_size 1024 --neg_sample_size 256 --hidden_dim 400 --gamma 143.0 \
+--lr 0.1 --num_thread 4 --regularization_coef 2.00E-06 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 --gpu 0 1 \
+--max_step 360000 --neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_emb --delimiter ,
+
+################## Script Result #################
+
+##################################################
+
+###FB3-RotatE:
+dglke_train --model_name RotatE --dataset Freebase --data_path ./data --format udd_hrt  \
+--data_files entity2id.txt relation2id.txt train.txt valid.txt test.txt   --batch_size 1024 --neg_sample_size 256 -de \
+--hidden_dim 200 --gamma 12.0 --lr 0.01 --regularization_coef 1e-7 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 \
+--gpu 0 1 --max_step 300000 --neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --num_thread 4 \
+--force_sync_interval 10000 --no_save_emb --delimiter , 
+
+################## Script Result #################
+		
+##################################################
