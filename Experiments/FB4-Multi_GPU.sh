27770cf19b0fd94678f7463da455a2848cf35fb7
Nasim Mahdavi <55268101+nasimshm@users.noreply.github.com>
2022-08-28 01:31:27 -0500

    Update FB4-Multi_GPU.sh

--- Experiments/FB4-Multi_GPU.sh
+++ Experiments/FB4-Multi_GPU.sh
@@ -5,7 +5,15 @@ dglke_train --model_name TransE_l2 --dataset Freebase --data_path ./data --forma
 --neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_em --delimiter ,
 
 ################## Script Result #################
-
+training takes 342.3868455886841 seconds
+-------------- Test result --------------
+Test average MRR : 0.6062352081069878
+Test average MR : 12.542271285475794
+Test average HITS@1 : 0.5154369403551373
+Test average HITS@3 : 0.6571414099256336
+Test average HITS@10 : 0.7716209971164061
+-----------------------------------------
+testing takes 271.252 seconds
 ##################################################
 
 ###FB4-DistMult:
@@ -15,7 +23,15 @@ dglke_train --model_name DistMult --dataset Freebase --data_path ./data --format
 --eval_interval 100000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --num_thread 4 --no_save_emb --delimiter ,
 
 ################## Script Result #################
-
+training takes 2661.189035654068 seconds
+-------------- Test result --------------
+Test average MRR : 0.8187715508637831
+Test average MR : 19.18095249042686
+Test average HITS@1 : 0.7778721137093453
+Test average HITS@3 : 0.8429859477145755
+Test average HITS@10 : 0.8908468526460356
+-----------------------------------------
+testing takes 843.701 seconds
 ##################################################
 
 ###FB4-ComplEx:
@@ -25,7 +41,15 @@ dglke_train --model_name ComplEx --dataset Freebase --data_path ./data --format 
 --max_step 360000 --neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_emb --delimiter ,
 
 ################## Script Result #################
-
+training takes 3559.9117217063904 seconds
+-------------- Test result --------------
+Test average MRR : 0.8993861574131577
+Test average MR : 16.93782000082095
+Test average HITS@1 : 0.8800847035500938
+Test average HITS@3 : 0.9102664226274519
+Test average HITS@10 : 0.9355053688399617
+-----------------------------------------
+testing takes 845.329 seconds
 ##################################################
 
 ###FB4-RotatE:
@@ -36,5 +60,13 @@ dglke_train --model_name RotatE --dataset Freebase --data_path ./data --format u
 --force_sync_interval 10000 --no_save_emb --delimiter , 
 
 ################## Script Result #################
-		
+training takes 7725.889139175415 seconds
+-------------- Test result --------------
+Test average MRR : 0.7299357109474364
+Test average MR : 33.02714078894227
+Test average HITS@1 : 0.6837280912702414
+Test average HITS@3 : 0.7535836715579973
+Test average HITS@10 : 0.8160462612280308
+-----------------------------------------
+testing takes 1123.047 seconds		
 ##################################################
