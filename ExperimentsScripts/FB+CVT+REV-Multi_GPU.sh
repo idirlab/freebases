60c4f3853ebb07a344edbdd7fb656deea2e2ef3c
Nasim Mahdavi <55268101+nasimshm@users.noreply.github.com>
2023-01-18 10:52:17 -0600

    Update and rename FB4-Multi_GPU.sh to FB+CVT+REV-Multi_GPU.sh

--- ExperimentsScripts/FB+CVT+REV-Multi_GPU.sh
+++ ExperimentsScripts/FB+CVT+REV-Multi_GPU.sh
@@ -1,19 +1,19 @@
 ###FB4_TransE_l2:
 dglke_train --model_name TransE_l2 --dataset Freebase --data_path ./data --format udd_hrt  \
 --data_files entity2id.txt relation2id.txt train.txt valid.txt test.txt --batch_size 1000 --neg_sample_size 200 --hidden_dim 400 --gamma 10 \
---lr 0.1 --regularization_coef 1e-9 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 --num_thread 4 --gpu 0 1  --max_step 320000 \
+--lr 0.1 --regularization_coef 1e-9 --batch_size_eval 1000 --test -adv --mix_cpu_gpu --num_proc 8 --num_thread 4 --gpu 0 1  --max_step 300000 \
 --neg_sample_size_eval 1000 --log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_em --delimiter ,
 
 ################## Script Result #################
-training takes 342.3868455886841 seconds
+training takes 4141.7184545993805 seconds
 -------------- Test result --------------
-Test average MRR : 0.6062352081069878
-Test average MR : 12.542271285475794
-Test average HITS@1 : 0.5154369403551373
-Test average HITS@3 : 0.6571414099256336
-Test average HITS@10 : 0.7716209971164061
+Test average MRR : 0.9708371778545675
+Test average MR : 1.464009902868417
+Test average HITS@1 : 0.9572764455911368
+Test average HITS@3 : 0.9828492563363181
+Test average HITS@10 : 0.9898030429503718
 -----------------------------------------
-testing takes 271.252 seconds
+testing takes 686.737 seconds
 ##################################################
 
 ###FB4-DistMult:
