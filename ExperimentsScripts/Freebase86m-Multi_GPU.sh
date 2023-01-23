99f7002ef9ab83ff36b7814a6632715c7fdcb5af
Nasim Mahdavi <55268101+nasimshm@users.noreply.github.com>
2023-01-23 14:05:21 -0600

    Update Freebase86m-Multi_GPU.sh

--- ExperimentsScripts/Freebase86m-Multi_GPU.sh
+++ ExperimentsScripts/Freebase86m-Multi_GPU.sh
@@ -2,7 +2,7 @@
 dglke_train --model_name TransE_l2 --dataset Freebase --batch_size 1000 --neg_sample_size 200 \
 --hidden_dim 400 --gamma 10 --lr 0.1 --regularization_coef 1e-9 --batch_size_eval 1000 \
 --test -adv --mix_cpu_gpu --num_proc 8 --num_thread 4 --gpu 0 1  --max_step 300000 --neg_sample_size_eval 1000 \
---log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_em &
+--log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --no_save_em 
 
 ################## Script Result #################
 #training takes 5180.231946229935 seconds
@@ -20,7 +20,7 @@ dglke_train --model_name TransE_l2 --dataset Freebase --batch_size 1000 --neg_sa
 dglke_train --model_name DistMult --dataset Freebase --batch_size 1024 --neg_sample_size 256 \
 --hidden_dim 400 --gamma 143.0 --lr 0.08 --batch_size_eval 1000 --test -adv --mix_cpu_gpu \
 --num_proc 8 --gpu 0 1 --max_step 300000 --neg_sample_size_eval 1000 --eval_interval 100000 \ 
---log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --num_thread 4 --no_save_emb &
+--log_interval 1000 --async_update --rel_part --force_sync_interval 10000 --num_thread 4 --no_save_emb 
 
 ################## Script Result #################
 #training takes 4898.2239985466 seconds
@@ -38,24 +38,34 @@ dglke_train --model_name DistMult --dataset Freebase --batch_size 1024 --neg_sam
 dglke_train --model_name ComplEx --dataset Freebase --batch_size 1024 --neg_sample_size 256 \
 --hidden_dim 400 --gamma 143.0 --lr 0.1 --num_thread 4 --regularization_coef 2.00E-06 --batch_size_eval 1000 \
 --test -adv --mix_cpu_gpu --num_proc 8 --gpu 0 1 --max_step 360000 --neg_sample_size_eval 1000 --log_interval 1000 \
---async_update --rel_part --force_sync_interval 10000 --no_save_emb &
+--async_update --rel_part --force_sync_interval 10000 --no_save_emb 
 
 ################## Script Result #################
-training takes 6434.67949962616 seconds
--------------- Test result --------------
-Test average MRR : 0.8350476460350464
-Test average MR : 46.558354334388625
-Test average HITS@1 : 0.8177202517669357
-Test average HITS@3 : 0.8420723398735495
-Test average HITS@10 : 0.8678930054317636
------------------------------------------
-testing takes 3861.581 seconds
+#training takes 6434.67949962616 seconds
+#-------------- Test result --------------
+#Test average MRR : 0.8350476460350464
+#Test average MR : 46.558354334388625
+#Test average HITS@1 : 0.8177202517669357
+#Test average HITS@3 : 0.8420723398735495
+#Test average HITS@10 : 0.8678930054317636
+#-----------------------------------------
+#testing takes 3861.581 seconds
 ##################################################
 
 ###Freebase86m_TransR:
+dglke_train --model_name TransR --dataset Freebase --batch_size 1024 --neg_sample_size 256 \
+--regularization_coef 5e-8 --hidden_dim 200 --gamma 8.0 --lr 0.04 --batch_size_eval 1000 --test -adv \
+--mix_cpu_gpu --num_proc 8 --gpu 0 1 --max_step 300000 --neg_sample_size_eval 1000 --log_interval 1000 --async_update \
+--rel_part --num_thread 4 --force_sync_interval 10000 --no_save_emb &
+
 ################## Script Result #################
 ##################################################
 
 ###Freebase86m_RotatE:
+dglke_train --model_name RotatE --dataset Freebase --batch_size 1024 --neg_sample_size 256 \
+-de --hidden_dim 200 --gamma 12.0 --lr 0.01 --regularization_coef 1e-7 --batch_size_eval 1000 \
+--test -adv --mix_cpu_gpu --num_proc 8 --gpu 0 1 --max_step 300000 --neg_sample_size_eval 1000 \
+--log_interval 1000 --async_update --rel_part --num_thread 4 --force_sync_interval 10000 --no_save_emb 
+
 ################## Script Result #################
 ##################################################
